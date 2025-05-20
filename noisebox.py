074c8ace6da1b781da79820f9d91f319dbe5882b
mravalanche <67553635+mravalanche@users.noreply.github.com>
2025-05-21 00:12:06 +0100

    wip

--- noisebox.py
+++ noisebox.py
@@ -257,7 +257,7 @@ disco_switch.when_deactivated = disco_stop
 def main():
     log.info("------- NoiseBox Starting -------")
     pygame.mixer.Sound.play(r2_beep_sound)
-    lightsaber_led2.blink()
+    lightsaber_led2.blink(0.25,0.25,4)
     pause()
 
 
